ab9201abd28b2fda40083059b588de3f63f06f21
Tadeo Alvarez <tadeo.regalado.alvarez@gmail.com>
2023-03-30 22:22:03 -0600

    creando conexion base de datos

---
+++ backend/db/db.js
@@ -0,0 +1,12 @@
+import { Sequelize } from "sequelize"; //usamos sequelize
+import dotenv from 'dotenv';
+dotenv.config(); //usamos variables de entorno
+
+// conexion base de datos
+const db = new Sequelize(process.env.DB_NOMBRE, process.env.DB_USUARIO, process.env.DB_USER_PASSWORD, {
+  host: process.env.DB_HOST,
+  dialect: 'postgres',
+  logging: false
+});
+
+export default db; //exportamos conexion
--- backend/index.js
+++ backend/index.js
@@ -1,12 +1,26 @@
+//deperencias
 import express, { json } from 'express';
 import dotenv from "dotenv";
 import cors from "cors";
-const app = express();
-const port = 3000 || process.env.PORT;
 
-app.use(json());
-app.use(cors());
-dotenv.config();
+//instancias
+import db from './db/db.js';
+
+//conexion base de datos
+try {
+    await db.authenticate();
+    console.log(`Base de datos conectada correctamente`);
+} catch (error) {
+    console.log(error);
+}
+
+//instancia de app express
+const app = express();
+const port = 3000 || process.env.PORT; //instancia de puerto
+
+app.use(json()); //habilitamos formato json
+app.use(cors()); //habilitamos cors
+dotenv.config(); //habilitamos variables de entorno
 
 app.get('/', (req, res) => {
     res.json({msg: "vivero app"})
