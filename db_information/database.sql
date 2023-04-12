1a7e2dd72e0e3e2aec2275ae9d10d0c2d2bbed84
Sebastian152 <sgalindorks@gmail.com>
2023-04-11 19:56:52 -0600

    Colocando NOT NULL en los atributos

--- db_information/database.sql
+++ db_information/database.sql
@@ -11,23 +11,23 @@ CREATE DATABASE vivero
 CREATE TABLE producto (
 	id_producto SERIAL PRIMARY KEY,
 	nombre VARCHAR(50) NOT NULL,
-	descripcion  VARCHAR(50) NOT NULL,
-	precio int,
-	cantidad int
+	descripcion VARCHAR(50) NOT NULL,
+	precio int NOT NULL,
+	cantidad int NOT NULL
 );
 
 CREATE TABLE sustrato (
 	id SERIAL PRIMARY KEY,
 	nombre VARCHAR(50) NOT NULL,
 	descripcion  VARCHAR(50) NOT NULL,
-	precio int,
-	cantidad int
+	precio int NOT NULL,
+	cantidad int NOT NULL
 );
 
 CREATE TABLE proveedor (
     id SERIAL PRIMARY KEY, 
     nombre VARCHAR(50) NOT NULL,
-    telefono VARCHAR(10)
+    telefono VARCHAR(10) NOT NULL
 );
 
 CREATE TABLE cliente (
@@ -37,8 +37,8 @@ CREATE TABLE cliente (
 
 CREATE TABLE sucursal (
 	id_sucursal SERIAL PRIMARY KEY,
-	direccion VARCHAR(50),
-	telefono VARCHAR(10),
+	direccion VARCHAR(50) NOT NULL,
+	telefono VARCHAR(10) NOT NULL,
 	email VARCHAR(30) NOT NULL,
 	hora_apertura TIME NOT NULL,
 	hora_cierre TIME NOT NULL
@@ -49,6 +49,6 @@ CREATE TABLE empleado (
 	nombre VARCHAR(30) NOT NULL,
 	hora_entrada TIME NOT NULL,
 	hora_salida TIME NOT NULL,
-	id_sucursal int,
+	id_sucursal int NOT NULL,
 	FOREIGN KEY (id_sucursal) REFERENCES sucursal(id_sucursal)
 );
