c26b9c4ae7c8cbe930478ce590da3f3463ce8acb
tadeo alvarez <tadeo.regalado.alvarez@gmail.com>
2023-04-11 18:44:07 -0600

    creando el modelo de producto

---
+++ backend/models/ProductoModel.js
@@ -0,0 +1,40 @@
+import { Sequelize, DataTypes, Model } from 'sequelize';
+
+//clase producto que extiende un modelo
+class Producto extends Model {
+    static init(sequelize) {
+        return super.init({
+            id: {
+                type: Sequelize.INTEGER,
+                primaryKey: true,
+                autoIncrement: true
+            },
+            nombre: {
+                type: DataTypes.STRING,
+                allowNull: false
+            },
+            descripcion: {
+                type: DataTypes.TEXT('long'),
+                allowNull: false
+            },
+            precio: {
+                type: DataTypes.FLOAT,
+                allowNull: false
+            },
+            cantidad: {
+                type: DataTypes.INTEGER,
+                allowNull: false
+            },
+        }, {
+            sequelize,
+            tableName: 'Producto', //nombre de la tabla
+            modelName: 'Producto', //nombre del modelo
+        });
+    }
+
+    static associate(models) {
+        Producto.belongsTo(models.Administrador, {foreignKey: 'id'})
+    }
+}
+
+export default Producto
