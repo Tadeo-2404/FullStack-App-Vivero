9a04ecc1b852682b339949a02b6d1cd77e2f0f07
tadeo alvarez <tadeo.regalado.alvarez@gmail.com>
2023-04-13 16:50:33 -0600

    organizando rutas /cliente y /admin, usando Layouts

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,33 +1,44 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+} from "react-router-dom";
 import "./App.css";
+import Inicio from "./pages/Inicio";
+import NotFound from "./pages/NotFound";
+import FormPublicarProducto from "./pages/admin/FormPublicarProducto";
+import FormEditarProducto from "./pages/admin/FormEditarProducto";
+import TemplateLayoutCliente from "./layout/TemplateLayoutCliente";
+import TemplateLayoutAdmin from "./layout/TemplateLayoutAdmin";
+import IniciarSesion from "./pages/cliente/IniciarSesion";
+import Sucursales from "./pages/cliente/Sucursales";
+import Registro from "./pages/cliente/Registro";
 
-import Inicio from './pages/Inicio';
-import NotFound from './pages/NotFound';
-import FormPublicarProducto from './pages/admin/FormPublicarProducto';
-import FormEditarProducto from './pages/admin/FormEditarProducto';
+function App() {
+  return (
+    <Router>
+        <Routes>
+          <Route path="/cliente" element={<TemplateLayoutCliente />}>
+            {/* CLIENTES */}
+            <Route index element={<Inicio />} />
+            <Route path="registrarse" element={<Registro />} />
+            <Route path="iniciar_sesion" element={<IniciarSesion />} />
+            <Route path="sucursales" element={<Sucursales />} />
+            <Route path="*" element={<NotFound />} />
+          </Route>
 
-import Navbar from './components/Navbar';
-
-function App(){
-    return(
-        <Router>
-            <Navbar />
-            <Routes>
-                {/* CLIENTES */}
-                {/* <Route path="/registro" element={<Registro />} /> */}
-                {/* <Route path="/inicio-sesion" element={<InicioSesion />} /> */}
-                <Route path="/" element={<Inicio />} />
-                {/* <Route path="/sucursales" element={<Sucursales />} /> */}
-
-                {/* ADMIN */}
-                {/* <Route path="/admin/inicio-sesion" element={<InicioSesionAdmin />} /> */}
-                <Route path="/admin/publicar" element={<FormPublicarProducto />} />
-                <Route path="/admin/editar/:id" element={<FormEditarProducto />} />
-                {/* <Route path="/admin/proveedores" element={<Proveedores />} /> */}
-                <Route path="*" element={<NotFound />} />
-            </Routes>
-        </Router>
-    )
+          <Route path="/admin" element={<TemplateLayoutAdmin />}>
+            {/* ADMIN */}
+            {/* <Route path="/admin/inicio-sesion" element={<InicioSesionAdmin />} /> */}
+            <Route index element={<Inicio />} />
+            <Route path="admin/publicar" element={<FormPublicarProducto />} />
+            <Route path="admin/editar/:id" element={<FormEditarProducto />} />
+            {/* <Route path="/admin/proveedores" element={<Proveedores />} /> */}
+            <Route path="*" element={<NotFound />} />
+          </Route>
+        </Routes>
+    </Router>
+  );
 }
 
 export default App;
