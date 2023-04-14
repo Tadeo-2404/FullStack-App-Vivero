de908c6f7fa0c139631b7464de57431f2293631a
Manuel <ferman5903@hotmail.com>
2023-04-13 18:28:16 -0600

    Link a proveedores en inicio

--- frontend/src/pages/Inicio.jsx
+++ frontend/src/pages/Inicio.jsx
@@ -6,9 +6,11 @@ function Inicio(){
             <h1>Página de inicio</h1>
             <h2>GALERIA DE PRODUCTOS</h2>
             <h2>Rutas de prueba para ver el funcionamiento</h2>
-            <Link to="/admin/editar/1">Prueba ruta editar producto 1</Link>
+            <Link to="/admin/editar/1">Prueba ruta editar producto 1 (admin)</Link>
             <br />
             <Link to="/sucursales">Sucursales</Link>
+            <br />
+            <Link to="/admin/proveedores">Proveedores (admin)</Link>
         </>
     )
 }
